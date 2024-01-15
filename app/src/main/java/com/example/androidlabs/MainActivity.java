ecf02625e73f5da330938f91f3e2c474ae166260
Jeanine Estelle <jeanine@gmail.com>
2024-01-15 12:16:10 -0500

    Lab6.1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.androidlabs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -10,17 +9,16 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
     private ImageView catImageView;
     private ProgressBar progressBar;
 
@@ -29,95 +27,94 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        catImageView = findViewById(R.id.imageView);
+        catImageView = findViewById(R.id.imageViews);
         progressBar = findViewById(R.id.progressBar);
 
-        CatImages catImagesTask = new CatImages();
-        catImagesTask.execute();
+        // Start loading cat images
+        new CatImages().execute();
     }
 
-    private class CatImages extends AsyncTask<Void, Integer, Void> {
-        private Bitmap currentCatPicture;
+    private class CatImages extends AsyncTask<Void, Integer, Bitmap> {
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            while (!isCancelled()) {
-                try {
-                    URL url = new URL("https://cataas.com/cat?json=true");
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.connect();
+        protected Bitmap doInBackground(Void... voids) {
+            try {
+                JSONObject jsonObject = new JSONObject(downloadJson("https://cataas.com/cat?json=true"));
+                String catId = jsonObject.getString("id");
+                String imageUrl = "https://cataas.com" + jsonObject.getString("url");
 
-                    InputStream inputStream = connection.getInputStream();
-                    StringBuilder response = new StringBuilder();
-                    int data;
-                    while ((data = inputStream.read()) != -1) {
-                        response.append((char) data);
+                File imageFile = new File(getFilesDir(), catId + ".jpg");
+                Bitmap catBitmap;
+
+                if (imageFile.exists()) {
+                    catBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+                } else {
+                    catBitmap = downloadImage(imageUrl);
+                    if (catBitmap != null) {
+                        saveImageToFile(catBitmap, catId);
                     }
-                    inputStream.close();
-
-                    JSONObject jsonObject = new JSONObject(response.toString());
-                    String catId = jsonObject.getString("id");
-                    String catImageUrl = jsonObject.getString("url");
-
-                    File localFile = new File(getFilesDir(), catId + ".jpg");
-                    if (localFile.exists()) {
-                        currentCatPicture = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                    } else {
-                        URL imageUrl = new URL(catImageUrl);
-                        HttpURLConnection imageConnection = (HttpURLConnection) imageUrl.openConnection();
-                        imageConnection.connect();
-                        InputStream imageInput = imageConnection.getInputStream();
-                        currentCatPicture = BitmapFactory.decodeStream(imageInput);
-
-                        FileOutputStream outputStream = new FileOutputStream(localFile);
-                        currentCatPicture.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-                        outputStream.flush();
-                        outputStream.close();
-                    }
-
-                    publishProgress(0);
-                    for (int i = 0; i < 100; i++) {
-                        try {
-                            publishProgress(i);
-                            Thread.sleep(30);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }  catch (IOException e) {
-                    // Handle IOException (specific to network operations)
-                    Log.e("CatImages", "IO Exception: " + e.getMessage());
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    // Handle JSONException (specific to JSON parsing)
-                    Log.e("CatImages", "JSON Exception: " + e.getMessage());
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    // Catch any other generic exceptions
-                    Log.e("CatImages", "General Exception: " + e.getMessage());
-                    e.printStackTrace();
                 }
+
+                return catBitmap;
+            } catch (Exception e) {
+                // Handle errors and log them
+                Log.e("CatImages", "Error loading cat image: " + e.getMessage(), e);
+                return null;
             }
-            return null;
         }
 
         @Override
         protected void onProgressUpdate(Integer... values) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        if (currentCatPicture != null) {
-                            catImageView.setImageBitmap(currentCatPicture);
-                        }
-                        progressBar.setProgress(values[0]);
-                    } catch (Exception e) {
-                        Log.e("CatImages", "Error in onProgressUpdate: " + e.getMessage());
-                        e.printStackTrace();
-                    }
-                }
-            });
+            progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null) {
+                catImageView.setImageBitmap(bitmap);
+            }else {
+                // Handle the case where no valid bitmap was loaded
+                Log.e("CatImages", "No valid bitmap was loaded.");
+            }
+        }
+
+        private String downloadJson(String urlString) throws Exception {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream inputStream = connection.getInputStream();
+            StringBuilder result = new StringBuilder();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                result.append(new String(buffer, 0, length));
+                publishProgress(0); // Indicate progress
+            }
+
+            inputStream.close();
+            connection.disconnect();
+            return result.toString();
+        }
+
+        private Bitmap downloadImage(String urlString) throws Exception {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream inputStream = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+            inputStream.close();
+            connection.disconnect();
+            return bitmap;
+        }
+
+        private void saveImageToFile(Bitmap bitmap, String catId) throws Exception {
+            File imageFile = new File(getFilesDir(), catId + ".jpg");
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.close();
         }
     }
 }
